7a57ad7cb52b6c66cb44ef97a6b2a75e9e494837
Ryan Kennedy <rkennedy9064@gmail.com>
2020-03-12 13:04:25 -0500

    More cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! This crate provides vga specific functions, data structures,
 //! and access to various registers.
+//!
+//! Memory addresses `0xA0000 -> 0xBFFFF` must be readable and writeable
+//! this crate to work properly.
 
 #![no_std]
 #![warn(missing_docs)]
